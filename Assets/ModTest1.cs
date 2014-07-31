ff8f7979f02ddb8221248bc470be0bef194ff3f9
Osinko <yoshinari_m@msn.com>
2014-07-31 22:57:05 +0900

    no message

---
+++ Assets/ModTest1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ModTest1 : MonoBehaviour
+{
+		void Start ()
+		{
+				print (Mathf.Pow (9, 7));
+				print (Mathf.Pow (9, 8));	//unityのpow関数で8乗が限界
+				print (Math.Pow (9, 8));
+				print (Math.Pow (9, 15));	//doubleの計算で15乗が限界
+				print (Math.Pow (9, 16));
+		}
+}
