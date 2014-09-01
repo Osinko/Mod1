26fcca4714e7f1f6bd27dd9dd3b3210d7c66ab80
Osinko <yoshinari_m@msn.com>
2014-09-01 19:39:07 +0900

    no message

---
+++ Assets/Length.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Length : MonoBehaviour
+{
+		int[] n = new int[4];
+
+		List<string> str = new  List<string> ();
+
+		void Start ()
+		{
+				n [0] = 0;
+				n [1] = 1;
+				n [2] = 2;
+				n [3] = 3;
+
+				print (n.Length);
+
+				str.Add ("1");
+				str.Add ("2");
+				print (str.Count);
+				print (str [1]);
+
+				for (int i = 0; i < str.Count; i++) {
+			
+				}
+
+		}
+	
+}
---
+++ Assets/LoopNum4.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoopNum4 : MonoBehaviour
+{
+
+		void Start ()
+		{
+				int r = 0;        // counter
+				int s = 0;        // bit-reversal of r/2<
+				int N = 256;      // N can be any power of 2
+				int N2 = N << 1;  // N<<1 == N*2
+		
+				do {
+						print (s);
+						r += 2;
+						s ^= N - (N / (r & -r));
+				} while (r < N2);
+		}
+	
+}
